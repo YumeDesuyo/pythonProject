9c1912f7041d46b8232004aa6c64a3abe0492f2e
Yume <0317lmk@gmail.com>
2024-02-12 21:05:49 +0800

    更新协程

--- 高性能异步爬虫/协程/协程.py
+++ 高性能异步爬虫/协程/协程.py
@@ -1,29 +1,32 @@
 import asyncio
 
 
-# async修饰的函数，调用后返回一个协程对象
 async def request(url):
     print('正在请求url:' + url)
-    print('请求成功' + url)
+    print('请求成功:' + url)
+    return url
 
 
+# async修饰的函数，调用后返回一个协程对象
 c = request('www.baidu.com')
 
-# # 创建一个时间循环对象
+
+# # 创建一个事件循环对象
 # loop = asyncio.get_event_loop()
 #
-# # 将循环对象注册到loop中，然后启动loop
+# # 将循环对象注册到loop中，并启动loop
 # loop.run_until_complete(c)
 
 # # task的创建
 # loop = asyncio.get_event_loop()
+# # 基于loop创建task对象
 # task = loop.create_task(c)
 #
 # print(task)
 #
 # # 将task任务注册到loop中
 # loop.run_until_complete(task)
-# print(task.result())
+# print(task)
 
 
 # # future的使用
@@ -33,5 +36,13 @@ c = request('www.baidu.com')
 # loop.run_until_complete(task)
 # print(task)
 
+# 定义回调函数
+def callback_fun(task):
+    print(task.result())
+
 
 # 绑定回调
+loop = asyncio.get_event_loop()
+task = loop.create_task(c)
+task.add_done_callback(callback_fun)
+loop.run_until_complete(task)
